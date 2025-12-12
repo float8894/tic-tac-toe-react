454e983c99cc9318cd5a233d45c0b8bb5b636bec
Abhishek Panchal <abhishekpanchal8894@gmail.com>
2025-12-13 00:00:48 +0530

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import Player from './components/Player.jsx';
+import GameBoard from './components/GameBoard.jsx';
+import { useState } from 'react';
+import Log from './components/Log.jsx';
+
+function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((currActivePlayer) =>
+      currActivePlayer === 'X' ? 'O' : 'X'
+    );
+    setGameTurns((prevTurns) => {
+      let currentPlayer = 'X';
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X')
+        currentPlayer = 'O';
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+      return updatedTurns;
+    });
+  }
+  return (
+    <main>
+      <div id='game-container'>
+        <ol id='players' className='highlight-player'>
+          <Player
+            isActive={activePlayer === 'X'}
+            initialName='Player 1'
+            symbol='X'
+          ></Player>
+          <Player
+            isActive={activePlayer === 'O'}
+            initialName='Player 2'
+            symbol='O'
+          ></Player>
+        </ol>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          turns={gameTurns}
+        ></GameBoard>
+      </div>
+      <Log></Log>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+  let buttonTitle = 'Edit';
+  let editablePlayerName = <span className='player-name'>{playerName}</span>;
+  if (isEditing) {
+    buttonTitle = 'Save';
+    editablePlayerName = (
+      <input type='text' required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li className={isActive ? 'active' : undefined}>
+      <span className='player'>
+        {editablePlayerName}
+        <span className='player-symbol'>{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{buttonTitle}</button>
+    </li>
+  );
+}
