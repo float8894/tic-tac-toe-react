80e055685c9227cd94e3c3b2d4a4bf2fe8650f58
Abhishek Panchal <abhishekpanchal8894@gmail.com>
2025-12-13 00:06:17 +0530

    derive state from props

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -8,7 +8,8 @@ const initialGameBoard = [
 export default function GameBoard({ onSelectSquare, turns }) {
   let gameBoard = initialGameBoard;
 
-  for (let [square, player] of turns) {
+  for (let turn of turns) {
+    const { square, player } = turn;
     const { row, col } = square;
     gameBoard[row][col] = player;
   }
@@ -31,7 +32,9 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={onSelectSquare}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
